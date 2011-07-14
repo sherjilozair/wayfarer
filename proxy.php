e531e6397f071f6506329b350e88e7afd3a75332
Sherjil Ozair <sherjilozair@gmail.com>
2011-07-14 08:03:12 +0530

    Curl method

--- proxy.php
+++ proxy.php
@@ -1,3 +1,23 @@
 <?
-    echo file_get_contents($_POST['url']);
+
+$options = array(
+        CURLOPT_RETURNTRANSFER => true,     // return web page
+        CURLOPT_HEADER         => false,    // don't return headers
+        CURLOPT_FOLLOWLOCATION => true,     // follow redirects
+        CURLOPT_ENCODING       => "",       // handle all encodings
+        CURLOPT_USERAGENT      => "spider", // who am i
+        CURLOPT_AUTOREFERER    => true,     // set referer on redirect
+        CURLOPT_CONNECTTIMEOUT => 120,      // timeout on connect
+        CURLOPT_TIMEOUT        => 120,      // timeout on response
+        CURLOPT_MAXREDIRS      => 10,       // stop after 10 redirects
+    );
+    
+    
+    $ch      = curl_init( $_POST['url'] );
+    curl_setopt_array( $ch, $options );
+    echo curl_exec( $ch );
+    curl_close( $ch );
+
+    
+    
 ?>
